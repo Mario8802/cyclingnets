2c23870930b02d7d778edfbd0c194193fa86133e
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2024-11-13 02:20:56 +0100

    Removed BikePost listing functionality for future reimplementation.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,36 @@
 from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import BikePost
+from .forms import BikePostForm
 
-class BikePostListView(ListView):
+# ListView to display all Bike Posts
+class BikePostListView(LoginRequiredMixin, ListView):
     model = BikePost
     template_name = 'posts/bikepost_list.html'
     context_object_name = 'bike_posts'
+    login_url = 'login'  # Redirect to login page if not authenticated
+
+
+# Function-based view to create a new Bike Post
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = BikePostForm(request.POST, request.FILES)  # Include request.FILES for image uploads
+        if form.is_valid():
+            bike_post = form.save(commit=False)
+            bike_post.posted_by = request.user  # Set the current logged-in user as the author
+            bike_post.save()
+            return redirect('posts:bikepost_list')  # Redirect to the list of bike posts
+    else:
+        form = BikePostForm()
+
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+# Function-based view to list all posts (alternative to the ListView)
+@login_required
+def list_posts(request):
+    bike_posts = BikePost.objects.all()
+    return render(request, 'posts/list_posts.html', {'bike_posts': bike_posts})
