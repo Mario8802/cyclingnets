5f9f1b498311cda85f3d3a280f11e3c43d28ef7b
Mario <144950133+Mario8802@users.noreply.github.com>
2024-12-13 15:37:40 +0100

    Update settings.py
    "Configured PostgreSQL database with SSL options for enhanced security. Database settings are now environment-driven using python-decouple, ensuring flexibility and improved security for deployment across different environments."

--- bike_connect/settings.py
+++ bike_connect/settings.py
@@ -106,6 +106,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE'),
+        },
     }
 }
 
